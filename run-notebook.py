f47831beb64fcaf9612b615b02671a3f8e21428f
Randy Sargent <randy.sargent@gmail.com>
2019-02-09 07:34:46 -0500

    run-notebook.py can report success/fail to datadog

--- run-notebook.py
+++ run-notebook.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
-import argparse, codecs, datetime, fcntl, json, os, pwd, re, signal, subprocess, sys, time, traceback, urllib2
+import argparse, codecs, datetime, fcntl, json, os, pwd, re, requests, signal, subprocess, sys, time, traceback
+
+parser = argparse.ArgumentParser()
+parser.add_argument('notebook', help='Notebook to run')
+parser.add_argument('--email-on-fail', default='randy.sargent@gmail.com', help='Email this address on fail or timeout')
+parser.add_argument('--timeout', type=int, default=1800, help='Timeout, in seconds.  (Kill notebook if it runs this long.)')
+args = parser.parse_args()
+
+notebook_path = os.path.abspath(sys.argv[1])
+
+def header():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + (', pid(%d)' % os.getpid())
+
+log_path = notebook_path + '.log'
+logfile = codecs.open(log_path, 'a', encoding='utf-8')
+logfile.write(header() + ': run-notebook.py %s\n' % args.notebook)
+logfile.flush()
 
 def timeout(signum, frame):
     raise Exception('Notebook running too long (%d seconds, --timeout); killing' % args.timeout)
@@ -12,11 +28,42 @@ def sendMail(toAddrs, subject, body):
     if p.wait():
         raise Exception('sendmail failed')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('notebook', help='Notebook to run')
-parser.add_argument('--email-on-fail', default='randy.sargent@gmail.com', help='Email this address on fail or timeout')
-parser.add_argument('--timeout', type=int, default=1800, help='Timeout, in seconds.  (Kill notebook if it runs this long.)')
-args = parser.parse_args()
+datadog_ok = 0
+datadog_warning = 1
+datadog_critical = 2
+
+def datadog_request(url, **params):
+    datadog_params = json.load(open('/usr/local/datadog/config.json'))
+    params['host_name'] = subprocess.check_output(['hostname']).decode('utf-8').strip()
+    params['timestamp'] = time.time()
+    response = requests.post(url, params=datadog_params, json=params)
+
+def datadog_operation_succeeded(**params):
+    datadog_request('https://api.datadoghq.com/api/v1/check_run', 
+                    check="op.end",
+                    status=datadog_ok,
+                    **params)
+    
+def datadog_operation_failed(**params):
+    datadog_request('https://api.datadoghq.com/api/v1/check_run', 
+                    check="op.end",
+                    status=datadog_critical,
+                    **params)
+
+def notebook_succeeded(msg):
+    logfile.write(header() + ': ' + msg + '\n')
+    if os.path.exists('/usr/local/datadog/config.json'):
+        datadog_operation_succeeded(message=msg, tags=["notebook:" + os.path.basename(notebook_path)])
+        logfile.write(header() + ': sent to datadog\n')
+
+def notebook_failed(msg):
+    logfile.write(header() + ': ' + msg + '\n')
+    if os.path.exists('/usr/local/datadog/config.json'):
+        datadog_operation_failed(message=msg, tags=["notebook:" + os.path.basename(notebook_path)])
+        logfile.write(header() + ': sent to datadog\n')
+    else:
+        sendMail([args.email_on_fail], 'FAILED: %s' % notebook_path, message)
+        logfile.write(header() + ': emailed\n')
 
 signal.signal(signal.SIGALRM, timeout)
 signal.alarm(args.timeout)
@@ -24,22 +71,12 @@ signal.alarm(args.timeout)
 def run_notebook():
     run_notebook_start_time = time.time()
 
-    def header():
-        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + (', pid(%d)' % os.getpid())
-
-    notebook_path = os.path.abspath(sys.argv[1])
-
-    log_path = notebook_path + '.log'
-    logfile = codecs.open(log_path, 'a', encoding='utf-8')
-    logfile.write(header() + ': run-notebook.py %s\n' % args.notebook)
-    logfile.flush()
 
     def get_username():
         try:
             return pwd.getpwuid(os.getuid())[0]
         except:
             return None
-
 
     logfile.write('Running as user %s\n' % get_username())
     logfile.write('Using python %s\n' % sys.executable)
@@ -63,13 +100,12 @@ def run_notebook():
         sys.stdout = sys.stderr = logfile
     
         def exec_ipynb(filename_or_url):
-            nb = (urllib2.urlopen(filename_or_url) if re.match(r'https?:', filename_or_url) else open(filename_or_url)).read()
-            jsonNb = json.loads(nb)
-            #check for the modified formatting of Jupyter Notebook v4
-            if(jsonNb['nbformat'] == 4):
-                exec '\n'.join([''.join(cell['source']) for cell in jsonNb['cells'] if cell['cell_type'] == 'code']) in globals()
+            nb = (requests.get(filename_or_url).json() if re.match(r'https?:', filename_or_url) else json.load(open(filename_or_url)))
+            if(nb['nbformat'] >= 4):
+                src = [''.join(cell['source']) for cell in nb['cells'] if cell['cell_type'] == 'code']
             else:
-                exec '\n'.join([''.join(cell['input']) for cell in jsonNb['worksheets'][0]['cells'] if cell['cell_type'] == 'code']) in globals()
+                src = [''.join(cell['input']) for cell in nb['worksheets'][0]['cells'] if cell['cell_type'] == 'code']
+            exec('\n'.join(src), globals())
     
         try:
             os.chdir(os.path.dirname(notebook_path))
@@ -77,17 +113,13 @@ def run_notebook():
         except:
             message = header() + ': Caught exception trying to run notebook %s: %s\n' % (notebook_path, sys.exc_info()[0])
             message += traceback.format_exc()
-            logfile.write(message)
-            logfile.flush()
 
             # Email error
-            sendMail([args.email_on_fail], 'FAILED: %s' % notebook_path, message)
-            return 1
-    
-        logfile.write(header() + ': run-notebook.py %s completed successfully after %d seconds\n' % (args.notebook, time.time() - run_notebook_start_time))
-        return 0
+            notebook_failed('run-notebook.py %s failed after %d seconds with message %s\n' % (args.notebook, time.time() - run_notebook_start_time, message))
+            return
+
+        notebook_succeeded('run-notebook.py %s completed successfully after %d seconds' % (args.notebook, time.time() - run_notebook_start_time))
     finally:
         fcntl.flock(lockfile, fcntl.LOCK_UN)
 
-sys.exit(run_notebook())
-
+run_notebook()
