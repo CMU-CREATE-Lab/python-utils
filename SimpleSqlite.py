ca3fa5aa4d7fcfedaddca4d56d715cb49b6bb301
Randy Sargent <randy.sargent@gmail.com>
2019-10-26 12:24:27 -0400

    SimpleSqlite: refactor retries; add fetchall functionality

--- SimpleSqlite.py
+++ SimpleSqlite.py
@@ -4,66 +4,68 @@ try:
     import thread
 except:
     import threading as thread
-    
+
 class SimpleSqlite:
     def __init__(self, database_path):
-        self._database_path = _database_path
+        self._database_path = database_path
         self._log_path = self._database_path + '.log'
         self._log_file = open(self._log_path, 'a')
         self._max_retries = 5 # Retry locking this many times
         self._lock_delay_secs = 2  # Sleep this long between attempts to lock
         self._conns = {}
+        
+    def _perform_with_retries(self, operation_name, fn):
+        self._log('%s start' % (operation_name))
+
+        for num_retries in range(0, self._max_retries):
+            try:
+                before = time.time()
+                ret = fn()
+                self._log('%s completed, duration %.1f ms' % 
+                          (operation_name, 1000 * (time.time() - before)))
+                return ret
+            except sqlite3.OperationalError as e:
+                self._log('%s generated sqlite3.OperationalError %s, duration %.1f ms, num_retries=%d' % 
+                          (operation_name, e, 1000 * (time.time() - before), num_retries))
+                if num_retries < self._max_retries - 1:
+                    self._log('Sleeping %f seconds before retrying' % self._lock_delay_secs)
+                    time.sleep(self._lock_delay_secs)
+                    continue
+                else:
+                    self._log('Too many retries, raising exception')
+                    raise
+            except Exception as e:
+                self._log('%s generated exception %s, duration %.1f ms' % 
+                          (operation_name, e, 1000 * (time.time() - before)))
+                raise
 
     def execute_write(self, cmd, args=()):
-        num_retries = 0
-        self._log('execute_write start %s' % cmd)
-
-        while num_retries < self._max_retries:
-            try:
-                before = time.time()
-                self._get_conn().execute(cmd, args)
-                self._log('execute_write completed, duration %.1f ms' % (1000 * (time.time() - before)))
-                return True
-            except sqlite3.OperationalError as e:
-                self._log('execute_write generated sqlite3.OperationalError %s, duration %.1f ms, num_retries=%d, sleeping %d seconds' % (e, 1000 * (time.time() - before), num_retries, self._lock_delay_secs))
-                num_retries += 1
-                time.sleep(self._lock_delay_secs)
-                self._log('execute_write start retry %s' % cmd)
-                continue
-            except Exception as e:
-                self._log('execute_write generated exception %s, duration %.1f ms' % (e, 1000 * (time.time() - before)))
-                raise
-        self._log('execute_write %s failed after %d retries, duration %.1f ms' % (cmd, num_retries, 1000 * (time.time() - before)))
-        return False
+        def write_fn():
+            self._get_conn().execute(cmd, args)
+            self._get_conn().commit()
+            return True
+        
+        return self._perform_with_retries('execute_write(%s)' % cmd, write_fn)
 
     def execute_read_fetchall(self, cmd, args=()):
-        num_retries = 0
-        self._log('execute_read_fetchall start %s' % cmd)
-
-        while num_retries < self._max_retries:
-            try:
-                before = time.time()
-                rows = self._get_conn().execute(cmd, args).fetchall()
-                self._log('execute_read_fetchall completed, duration %.1f ms' % (1000 * (time.time() - before)))
-                return rows
-            except sqlite3.OperationalError as e:
-                self._log('execute_read_fetchall sqlite3.OperationalError %s, duration %.1f ms, num_retries=%d, sleeping %d seconds' % (e, 1000 * (time.time() - before), num_retries, self._lock_delay_secs))
-                num_retries += 1
-                time.sleep(self._lock_delay_secs)
-                self._log('execute_read_fetchall start retry %s' % cmd)
-                continue
-            except Exception as e:
-                self._log('execute_read_fetchall generated exception %s, duration %.1f ms' % (e, 1000 * (time.time() - before)))
-                raise
-                
-        self._log('execute_read_fetchall %s failed after %d retries, duration %.1f ms' % (cmd, num_retries, 1000 * (time.time() - before)))
-        return None
+        def read_fetchall_fn():
+            return self._get_conn().execute(cmd, args).fetchall()
+            
+        return self._perform_with_retries('execute_read_fetchall(%s)' % cmd, read_fetchall_fn)
     
+    def execute_read_fetchall_dicts(self, cmd, args=()):
+        def read_fetchall_dicts_fn():
+            cursor = self._get_conn().execute(cmd, args)
+            keys = [col[0] for col in cursor.description]
+            return [dict(zip(keys, row)) for row in cursor.fetchall()]
+        
+        return self._perform_with_retries('execute_read_fetchall_dicts(%s)' % cmd, read_fetchall_dicts_fn)
+
     def _get_TPID(self):
         return '%d.%s' % (os.getpid(), thread.get_ident())
 
     def _log(self, msg):
-        self._log_file.write('%s %s: %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], self.getTPID(), msg))
+        self._log_file.write('%s %s: %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], self._get_TPID(), msg))
         self._log_file.flush()
 
     def _get_conn(self):
