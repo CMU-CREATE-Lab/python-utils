631fa35ca7c95033508b9d5fca6e62f979041ac2
Randy Sargent <randy.sargent@gmail.com>
2020-06-10 18:58:37 -0400

    test_psql tests multithreading

--- test_psql.py
+++ test_psql.py
@@ -1,11 +1,23 @@
 #%%
-from psql import Psql
-import psycopg2, unittest
+import json, os, psycopg2, threading, unittest
 import pandas as pd
+
+if 'reload_module' in vars():
+    reload_module('utils')
+from utils import *
+
+reload_module('psql')
+from psql import Psql
 
 class TestPsql(unittest.TestCase):
     def setUp(self):
-        self.db = Psql(dbname='earthtime')
+        db_config = os.path.join(os.path.dirname(__file__), '.test_psql_credentials.json')
+        if os.path.exists(db_config):
+            args = json.load(open(db_config))
+        else:
+            print(f'{db_config} does not exist; using default database settings')
+            args={'dbname':'postgres'}
+        self.db = Psql(**args)
 
     def test_sanitize_column_name(self):
         self.assertEqual(self.db.sanitize_column_name(' Four score, and 40 years ago...'), 'four_score_and_40_years_ago')
@@ -56,16 +68,6 @@ class TestPsql(unittest.TestCase):
 
         self.assertEquals(None, self.db.select_record_or_none('SELECT * FROM test_df WHERE i=999'))
 
-    # TODO: implement date parsing
-    #def test_df_date(self):
-    #    df = pd.DataFrame([dict(d=datetime.date(2020,1,1))])
-    #    self.db.execute('DROP TABLE IF EXISTS test_df')
-    #    self.db.create_empty_table_from_df('test_df', df)
-    #    self.db.append_df_to_table(df, 'test_df')
-    #    df2 = self.db.select_as_df('SELECT * FROM test_df')
-    #    self.assertEquals(df.d[0], df2.d[0])
-    #    self.assertTrue(df.equals(df2))
-
     def test_insert(self):
         self.db.execute('DROP TABLE IF EXISTS foo')
         self.db.execute('CREATE TABLE foo (i int8, txt text)')
@@ -73,6 +75,42 @@ class TestPsql(unittest.TestCase):
         self.db.insert_record('foo', rec)
         rec2 = self.db.select_record('SELECT * FROM foo')
         self.assertEquals(rec, rec2)
+
+    def test_threads(self):
+        x = 0
+        lock = threading.Lock()
+
+        def read_x():
+            return self.db.select_record("SELECT v from foo where k='x'")['v']
+
+        def test(i):
+            nonlocal x
+            time.sleep(i * 0.3 % 1)
+            with lock:
+                self.assertEquals(x, read_x())
+            time.sleep(i * 0.35 % 0.5)
+            with lock:
+                x += 1
+                self.db.execute("UPDATE foo SET v = %s WHERE k = 'x'", (x,))
+            with lock:
+                self.assertEquals(x, read_x())
+        
+        def test_thread(i):
+            test(i)
+            with self.db.transaction():
+                test(i)
+            test(i)
+        
+        self.db.execute('DROP TABLE IF EXISTS foo')
+        self.db.execute('CREATE TABLE foo (k text, v int)')
+        self.db.execute("INSERT INTO foo VALUES ('x', 0)")
+        thcalls = [ThCall(test_thread, i) for i in range(10)]
+        [thcall.value() for thcall in thcalls]
+        self.assertEquals(30, read_x())
+
+
+        # ThCall(func, *args, **kwargs) calls func(*args, **kwargs) in a separate thread
+# value() waits for func to complete and returns its value
 
 if __name__ == '__main__':
     if 'get_ipython' in vars():
